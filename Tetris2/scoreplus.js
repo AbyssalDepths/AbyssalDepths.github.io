f1ed325f04b172fa2c5c19dba78c235475415c60
AbyssalDepths <64440682+AbyssalDepths@users.noreply.github.com>
2022-07-21 20:55:59 -0300

    Update scoreplus.js

--- Tetris2/scoreplus.js
+++ Tetris2/scoreplus.js
@@ -1,4 +1,4 @@
-url = "wss://ucp-games-2021.azurewebsites.net/multiplayer";
+url = "wss://javascript.info/chat";
   window.multiplayer = new WebSocket(url);
   window.playerID = localStorage.getItem('playerID'); 
    if (typeof window.playerID === 'undefined' || window.playerID == null) { 
