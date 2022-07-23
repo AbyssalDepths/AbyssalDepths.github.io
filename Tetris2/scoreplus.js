ec8642934693bd6693e7fbc901e2951a6d1c73e5
AbyssalDepths <64440682+AbyssalDepths@users.noreply.github.com>
2022-07-23 11:16:09 -0300

    Update scoreplus.js

--- Tetris2/scoreplus.js
+++ Tetris2/scoreplus.js
@@ -16,7 +16,7 @@ url = "wss://ucp-games-2021.azurewebsites.net/multiplayer";
   };
 
 
-// hola function isOpen(ws) { return ws.readyState === ws.OPEN }aaaaaaaaa
+// hola function isOpen(ws) { return ws.readyState === ws.OPEN }aaaaaaaaa123
 
 function scoreplus(puntos){
   var datosprueba = {
